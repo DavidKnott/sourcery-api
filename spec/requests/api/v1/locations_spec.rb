ffaf5c48634decac7886cce906343ec84af74be8
David Knott <david1k1nott@gmail.com>
2017-03-29 18:35:25 -0600

    create failing location show api test
    [#142639315]

--- spec/requests/api/v1/locations_spec.rb
+++ spec/requests/api/v1/locations_spec.rb
@@ -9,4 +9,16 @@ describe "Locatins API" do
     expect(Location.first.physical_address).to eq "Cr 70C No. 4-42 OF 203, Medellín"
     expect(Location.first.ethereum_address).to eq "0x7da07904ffc79d7a69afdecb7b1eb0d767b37803"
   end
+
+  it "can return a location given its address" do
+    expected_location = create(:location, creator: "John", physical_address: "Cr 70C No. 4-42 OF 203, Medellín", ethereum_address: "0x7da07904ffc79d7a69afdecb7b1eb0d767b37803")
+    get "/api/v1/locations/#{expected_location.ethereum_address}"
+
+    actual_location = JSON.parse(response.body)
+
+    expect(response.status).to eq 200
+    expect(actual_location["creator"]).to eq "John"
+    expect(actual_location["physical_address"]).to eq "Cr 70C No. 4-42 OF 203, Medellín"
+    expect(actual_location["ethereum_address"]).to eq "0x7da07904ffc79d7a69afdecb7b1eb0d767b37803"
+  end
 end
