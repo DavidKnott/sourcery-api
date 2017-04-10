3383e158edfeaf1debc08eddd3105130428e4ad3
David Knott <david1k1nott@gmail.com>
2017-04-10 13:34:02 -0600

    Create root path that lists api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
   namespace :api do
     namespace :v1 do
       resources :products, only: [:create, :show, :index]
