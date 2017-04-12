a49e7bcbd9af3ecd5bb86caa4fe24b2d4714245b
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-12 08:09:45 -0600

    Failing spec moving UID logic from Lot to Checkpoint

--- spec/models/checkpoint_spec.rb
+++ spec/models/checkpoint_spec.rb
@@ -1,16 +1,60 @@
 require 'rails_helper'
 
 describe Checkpoint do
+  context 'database' do
+    it { is_expected.to have_db_column(:id) }
+    it { is_expected.to have_db_column(:creator) }
+    it { is_expected.to have_db_column(:street_address) }
+    it { is_expected.to have_db_column(:city) }
+    it { is_expected.to have_db_column(:state) }
+    it { is_expected.to have_db_column(:country) }
+    it { is_expected.to have_db_column(:ethereum_address) }
+    it { is_expected.to have_db_column(:lat) }
+    it { is_expected.to have_db_column(:lng) }
+    it { is_expected.to have_db_column(:zipcode) }
+  end
+
   context "validations" do
+    it { is_expected.to validate_uniqueness_of(:ethereum_address) }
+    it { is_expected.to validate_length_of(:ethereum_address).is_at_least(42).is_at_most(42) }
     it { is_expected.to validate_presence_of(:creator) }
     it { is_expected.to validate_presence_of(:street_address) }
     it { is_expected.to validate_presence_of(:city) }
     it { is_expected.to validate_presence_of(:state) }
     it { is_expected.to validate_presence_of(:country) }
-    it { is_expected.to validate_presence_of(:ethereum_address) }
     it { is_expected.to validate_presence_of(:lat) }
     it { is_expected.to validate_presence_of(:lng) }
     it { is_expected.to validate_presence_of(:zipcode) }
+  end
+
+  describe 'ethereum_address' do
+    let(:checkpoint) { Checkpoint.new }
+    let(:bad_start_checkpoint) { '1x1234567890123456789012345678901234567890' }
+    let(:bad_chars_checkpoint) { '0x?????67890123456789012345678901234567890' }
+    let(:last_40) { SecureRandom.hex(20) }
+
+    it 'is generated upon save' do
+      checkpoint.save
+      expect(checkpoint.ethereum_address.length).to eq 42
+    end
+
+    context 'validations' do
+      it "is invalid when it does not start with '0x'" do
+        checkpoint.ethereum_address = bad_start_checkpoint
+        expect(checkpoint).not_to be_valid
+      end
+
+      it "is invalid when it uses anything other than hexadecimal characters" do
+        checkpoint.ethereum_address = bad_chars_checkpoint
+        expect(checkpoint).not_to be_valid
+      end
+
+      it "is valid when well formed" do
+        valid_checkpoint = create(:checkpoint)
+
+        expect(valid_checkpoint).to be_valid
+      end
+    end
   end
 
   describe ".get_details_for" do
