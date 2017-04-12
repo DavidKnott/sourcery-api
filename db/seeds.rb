c35646884df4f6a24d0ef019849e45ad8805ae3c
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-12 15:50:16 -0600

    Add some ‘Lots’ to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+Lot.destroy_all
 Product.destroy_all
 Checkpoint.destroy_all
 
 10.times do
   FactoryGirl.create(:product)
   FactoryGirl.create(:checkpoint)
+  5.times { FactoryGirl.create(:lot, product: Product.all.sample) }
 end
 
 FactoryGirl.create(:checkpoint, ethereum_address: '0x4a48d76b56f16fe9200bcc6a7fc8e85cf8e4f7b1')
