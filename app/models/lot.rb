fee5bf8ac2915d7aba95dd845d309819a6633c81
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-12 08:07:38 -0600

    Remove UID from Lots
    Originally lots were going to need Ethereum
    addresses.  However, we only need a lot_id because
    of how we implemented the blockchain contracts.
    
    The result is that this UID logic will get moved 
    over to Checkpoint as the ethereum_address.

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,26 +1,3 @@
 class Lot < ApplicationRecord
-  validates_length_of :uid, minimum: 42, maximum: 42
-  validates_uniqueness_of :uid
-  validate :uid_start
-  validate :uid_hex
   belongs_to :product
-  before_validation :generate_uid
-
-  private 
-
-    def generate_uid
-      self.uid ||= '0x' + SecureRandom.hex(20)
-    end
-
-    def uid_start
-      if uid[0..1] != '0x'
-        errors.add(:uid, "must start with '0x'")
-      end
-    end
-
-    def uid_hex
-      unless uid[2..-1].chars.all? { |letter| letter =~ /\h/ }
-        errors.add(:uid, "must be composed of hexidecimal characters only")
-      end
-    end
 end
