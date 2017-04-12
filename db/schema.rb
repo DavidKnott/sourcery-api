ed56a42d9939c5293c8cb81c9c6b0edbe36b40b7
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-12 08:08:59 -0600

    Migration and schema for removing UID from Lots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411235440) do
+ActiveRecord::Schema.define(version: 20170412130133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,12 +30,10 @@ ActiveRecord::Schema.define(version: 20170411235440) do
   end
 
   create_table "lots", force: :cascade do |t|
-    t.string   "uid"
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_lots_on_product_id", using: :btree
-    t.index ["uid"], name: "index_lots_on_uid", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
