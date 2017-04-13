ca61e23f5720922a0dc7eec955b1a92da5da1a1f
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-13 13:15:39 -0600

    Return a product’s lot ids via API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root "home#index"
   namespace :api do
     namespace :v1 do
-      resources :products, only: [:create, :show, :index]
+      resources :products, only: [:create, :show, :index] do
+        resources :lots, only: [:index]
+      end
       resources :checkpoints, only: [:create, :show], key: :ethereum_address
       get "get_checkpoints_for_good_path", to: "get_checkpoints_for_good_path#index"
       resources :lots, only: [:create]
