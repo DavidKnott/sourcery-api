7bc65091bcd9b4a4e1fe73b1eb8365a7a28c5516
Mike Schutte <tmikeschutte@gmail.com>
2017-04-25 15:45:20 -0600

    Update get checkpoints for path controller
    * Add an || operator to set the addresses to [] if none are send
    * Add a special return for empty data
    * Add spec for sad path

--- app/controllers/api/v1/get_checkpoints_for_good_path_controller.rb
+++ app/controllers/api/v1/get_checkpoints_for_good_path_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::GetCheckpointsForGoodPathController < ApplicationController
   def index
-    checkpoints = Checkpoint.get_details_for(params[:checkpoints])
-    if checkpoints
-      render json: checkpoints
+    checkpoints = Checkpoint.get_details_for(params[:checkpoints] || [])
+    if checkpoints != false
+      return render json: checkpoints if checkpoints.any?
+      render json: { message: "No addresses sent" }, status: 400
     else
       render json: {
         invalid_addresses: Checkpoint.invalid_addresses(params[:checkpoints]),
